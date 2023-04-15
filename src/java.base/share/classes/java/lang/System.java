4f44ea867dc58cd798e6350acc6b734bb59153ea
Nikolai Korobeinikov <ricnorr@yandex-team.ru>
2023-04-15 16:01:18 +0300

    sol

--- src/java.base/share/classes/java/lang/System.java
+++ src/java.base/share/classes/java/lang/System.java
@@ -2620,8 +2620,7 @@ public final class System {
                 if (thread instanceof VirtualThread vthread) {
                     vthread.unparkAndRunOnThisCarrier(carrier);
                 } else {
-                    System.out.println(thread);
-                    throw new WrongThreadException();
+                    throw new WrongThreadException(thread.toString());
                 }
             }
 
